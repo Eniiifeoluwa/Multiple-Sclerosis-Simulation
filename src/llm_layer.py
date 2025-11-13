084524ee55332e5b89657da4c75ab5ab91fb0d85
unknown <akinolaayomide226@gmail.com>
2025-11-13 09:16:41 +0100

    Requirements edited.

--- src/llm_layer.py
+++ src/llm_layer.py
@@ -1,6 +1,6 @@
 from langchain_groq import ChatGroq
 from langchain_core.prompts import PromptTemplate
-from langchain.chains import LLMChain
+from langchain_classic.chains import LLMChain
 from langchain.memory import ConversationBufferMemory  # persistent conversation memory
 import sys, os
 sys.path.append(os.path.dirname(__file__))
